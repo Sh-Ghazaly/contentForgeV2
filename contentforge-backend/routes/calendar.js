23c22eafeeced1e9fcab8bccd844f90e76c3b687
abdulrhman_eid <116203726+abdo1717@users.noreply.github.com>
2026-06-19 17:54:04 +0300

    Add User model import to calendar routes

--- contentforge-backend/routes/calendar.js
+++ contentforge-backend/routes/calendar.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
 const { Trend } = require("../models");
-const { Brand, Calendar, Post, OriginalCalendar } = require("../models");
+const { Brand, Calendar, Post, OriginalCalendar, User } = require("../models");
 const {
   incrementUsage,
   checkCalendarLimit,
